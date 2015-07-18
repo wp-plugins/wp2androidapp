727f34cfaf7fc0043407c54d2b78f4b301e29fc0
sangvish <sangvish@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-18 19:36:21 +0000

    first build
    git-svn-id: https://plugins.svn.wordpress.org/wp2androidapp@1201765 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,71 @@
+=== wordpress2androidapp ===
+Contributors: sangvish
+Tags: android, android app, android blog app, wordpress to android app, wordpress android app,convert wordpress to android app, build WordPress app, create blog app, create mobile app
+Requires at least: 3.3 or higher
+Tested up to: 4.2.2
+Stable tag: trunk
+Author: sangvish technologies
+Author URI: http://sangvish.com/
+Plugin URI: http://freeweb2app.com 
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Donate link: http://freeweb2app.com/donate-us/
+
+Convert your wordpress website into android app instantly
+
+== Description ==
+convert your WordPress site into mobile app for Android. 
+Its very easy to use, with no coding required.
+
+Step:
+1) Just fill the form (required fields -> app name,app description)
+2) upload lcon for app
+3) upload splash screen image
+That\'s all. 
+
+Is this free?
+Its fully free of cost.
+
+Shortcode: [freeweb2app]
+
+
+Main features:
+1) Enable your mobile users to access your mobile site from their mobiles.
+2) Users can easily read and post new comments directly from your mobile Apps.
+3) Display your images and galleries in a mobile App 
+4) Playing video and audio using mobile players.
+5) Readers can easily search your content directly from your mobile iPhone App.
+Support multiple languages.
+6) Use Adsense ads for mobile to monetize your Apps.
+7) Push Notification
+8) Track app usage using google analytic
+9) Compatible with Woocommerce powered shop.
+10) Compatible with BuddyPress
+11) Earn money via google Admob
+12) Rate My App 
+
+
+How to publish in google play store: 
+https://play.google.com/apps/publish/signup/
+
+
+== Screenshots ==
+1. create app screensot. 
+
+== Installation ==
+1. Upload wordpress2androidapp plugin to your blog.
+2. Activate it.
+3. Goto Freeweb2app menu in your admin panel.
+
+== Changelog ==
+1.0 initial release
+
+== Upgrade Notice ==
+1.0 Initial release
+
+== Frequently Asked Questions ==
+is this free?
+Yes its absolutely free
+
+shortcode to display app creation form in my website?
+[freeweb2app]
---
+++ trunk/web2app.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>create an android app within 5 minutes - convert your website into android app</title>
+
+
+<style type="text/css">
+.clearfix:after {
+     visibility: hidden;
+     display: block;
+     font-size: 0;
+     content: " ";
+     clear: both;
+     height: 0;
+     }
+.clearfix { display: inline-block; }
+/* start commented backslash hack \*/
+* html .clearfix { height: 1%; }
+.clearfix { display: block; }
+/* close commented backslash hack */
+.fwa_left{ float:left;  width:70%;}
+.fwa_right{ float:left; width:20%; padding:0 20px; }
+.fwa_ad{ margin-bottom:30px; width:100%; }
+
+@media only screen and (max-device-width: 480px){
+.fwa_left{ float:left;  width:100% !important; padding:20px; }
+.fwa_right{ float:left; width:100% !important; padding:20px;  }
+
+}
+</style>
+</head>
+<body>
+<div class="fwa_option">
+<p> <strong>Note: </strong> Website2androidapp plugin convert your WordPress site into mobile app for Android. Its very easy to use, with no coding required. <br/> <br/> 
+Use below shortcode to show in your website. <br/> <h3>Shortcode: [freeweb2app] </h3> </p>
+<div class="clearfix">
+<div class="fwa_left">
+<iframe src="http://freeweb2app.com/create-android-app" style="min-width:100%; min-height:800px" ></iframe>
+<br/> <br/>
+<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FSangVishTechnologies&amp;width=400&amp;height=710&amp;colorscheme=light&amp;show_faces=true&amp;stream=true&amp;header=false&amp;appId=185450134846732" scrolling="no" frameborder="0" style="border-bottom:1px solid #3b5998; overflow:hidden;width: 400px;    height: 710px;background: #fff;margin-top: 3px;" allowtransparency="true"></iframe>
+<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fletswechat&amp;width=400&amp;height=710&amp;colorscheme=light&amp;show_faces=true&amp;stream=true&amp;header=false&amp;appId=185450134846732" scrolling="no" frameborder="0" style="border-bottom:1px solid #3b5998; overflow:hidden;width: 400px;    height: 710px;background: #fff;margin-top: 3px;" allowtransparency="true"></iframe>
+</div>
+
+<div class="fwa_right">
+
+<div class="fwa_ad">
+<a href="http://freeweb2app.com/contact/" target="_blank"><?php echo ' <img src="' . plugins_url( 'assets/images/2.gif', __FILE__ ) . '"  align="center" alt="ads" width="300px" height="auto" /> '; ?> </a>
+</div>
+
+<div class="fwa_ad">
+<a href="http://sangvish.com" target="_blank"> <?php echo ' <img src="' . plugins_url( 'assets/images/1.gif', __FILE__ ) . '"  align="center" alt="ads" width="300px" height="auto" /> '; ?> </a>
+</div>
+
+<div class="fwa_ad">
+<a href="http://letswechat.com" target="_blank"><?php echo ' <img src="' . plugins_url( 'assets/images/3.gif', __FILE__ ) . '"  align="center" alt="ads" width="300px" height="auto" /> '; ?> </a>
+</div>
+
+<div class="fwa_ad">
+<a href="http://digimarket.sangvish.com" target="_blank"><?php echo ' <img src="' . plugins_url( 'assets/images/4.jpg', __FILE__ ) . '"  align="center" alt="ads" width="300px" height="auto" /> '; ?> </a>
+</div>
+
+<div class="fwa_ad">
+<a href="http://freeweb2app.com" target="_blank"><?php echo ' <img src="' . plugins_url( 'assets/images/5.jpg', __FILE__ ) . '"  align="center" alt="ads" width="300px" height="auto" /> '; ?> </a>
+</div>
+
+<div class="fwa_ad">
+<a href="http://sangvish.com/contact-us/" target="_blank"><?php echo ' <img src="' . plugins_url( 'assets/images/6.gif', __FILE__ ) . '"  align="center" alt="ads" width="300px" height="auto" /> '; ?> </a>
+</div>
+
+
+<div class="fwa_fb">
+
+</div>
+
+</div>
+
+</div>
+
+</body>
+</html>
